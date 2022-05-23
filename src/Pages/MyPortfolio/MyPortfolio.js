3ab83318095b8f1b8d07c50da41578dd2acc398a
jubaer ibn zohir <jubaeribnzohir@gmail.com>
2022-05-23 22:08:55 +0600

    started the projects

---
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyPortfolio = () => {
+    return (
+        <div>
+            <h2>My name is Jubaer ibn zohir</h2>
+        </div>
+    );
+};
+
+export default MyPortfolio;
