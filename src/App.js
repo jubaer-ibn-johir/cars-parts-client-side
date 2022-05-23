a4390f046f1f5982f3f9828b72852cb36e720fbd
jubaer ibn zohir <jubaeribnzohir@gmail.com>
2022-05-23 22:44:56 +0600

    nav added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import MyPortfolio from './Pages/MyPortfolio/MyPortfolio';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
-    <div>
+    <div className='mx-4'>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="portfolio" element={<MyPortfolio />} />
+        <Route path="login" element={<Login />} />
       </Routes>
 
     </div>
