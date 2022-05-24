0ae2395b6b81aef5c2f14dba9b2f372f7246cda4
jubaer ibn zohir <jubaeribnzohir@gmail.com>
2022-05-25 03:24:23 +0600

    done for today

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -5,7 +5,7 @@ const ContactUs = () => {
         <section
        className=" bg-gray-200 rounded my-28 mx-6 justify-center items-center pt-20 text-center "
     >
-      <h3  className="text-xl text-primary font-bold ">Contact Us</h3>
+      <h3  className="text-2xl text-black font-bold ">Contact Us</h3>
       <h2  className="text-3xl m-4 ">Stay connected with us</h2>
       <input
         type="name"
